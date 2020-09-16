c5146b8f76aa17b1973bd8b2a0383c6e0383ecab
Aftab 'SHUR1KEN' Alam <42811989+SHUR1K-N@users.noreply.github.com>
2020-09-16 18:53:59 +0530

    Moved initialization of colors to the top-level, added keyboard interrupt exception handling, general code clean-up wave things

--- RARNinja.py
+++ RARNinja.py
@@ -3,6 +3,8 @@ from colorama import init
 from termcolor import colored
 import rarfile
 
+init() # Initilizing colors
+
 correctPath = False
 rarfile.UNRAR_TOOL = "UnRAR.exe"
 
@@ -21,7 +23,6 @@ BANNER3 = colored('''                                   ------------------------
 
 
 def printBanner():
-    init()
     print(BANNER1), print(BANNER2), print(BANNER3)
 
 
@@ -75,51 +76,58 @@ if __name__ == "__main__":
 
     printBanner()
 
-    while (True):
-        RAR = input("\nEnter RAR file path here: ")
-        dictionary = input("Enter dictionary file path here: ")
+    try:
 
-        if (os.path.isfile(RAR) is True and os.path.isfile(dictionary) is True):
-            break
+        while (True):
+            RAR = input("\nEnter RAR file path here: ")
+            dictionary = input("Enter dictionary file path here: ")
+
+            if (os.path.isfile(RAR) is True and os.path.isfile(dictionary) is True):
+                break
+            else:
+                clrscr()
+                print("\nEither file does not exist or invalid path entered. Try again.\n")
+                continue
+
+        while (True):
+            print("\nShow progress?")
+            print("1. Yes (slower)\n2. No (faster)")
+            progressPrompt = input("\nSelect option number (Default = No): ") or "2"
+
+            if (progressPrompt == "1"):
+                start = time.time()
+                found, tries = progress()
+                completionTime = time.time() - start
+                break
+
+            elif (progressPrompt == "2"):
+                start = time.time()
+                found, tries = noProgress()
+                completionTime = time.time() - start
+                break
+
+            else:
+                clrscr()
+                print("\nInvalid entry. Choose either option 1 or 2. Try again.\n")
+                continue
+
+        if found:
+            try:
+                rate = (int(tries) // completionTime)
+                print(f"\n\nThe task completed successfully in {completionTime} seconds. (at ~{rate} tries/sec)")
+                print("Press any key to exit.")
+                input()
+
+            except ZeroDivisionError:
+                print("\n\nThe task completed successfully in zero seconds.")
+                print("Press any key to exit.")
+                input()
         else:
-            clrscr()
-            print("\nEither file does not exist or invalid path entered. Try again.\n")
-            continue
-
-    while (True):
-        print("\nShow progress?")
-        print("1. Yes (slower)\n2. No (faster)")
-        progressPrompt = input("\nSelect option number (Default = No): ") or "2"
-
-        if (progressPrompt == "1"):
-            start = time.time()
-            found, tries = progress()
-            completionTime = time.time() - start
-            break
-
-        elif (progressPrompt == "2"):
-            start = time.time()
-            found, tries = noProgress()
-            completionTime = time.time() - start
-            break
-
-        else:
-            clrscr()
-            print("\nInvalid entry. Choose either option 1 or 2. Try again.\n")
-            continue
-
-    if found:
-        try:
-            rate = (int(tries) // completionTime)
-            print(f"\n\nThe task completed successfully in {completionTime} seconds. (at ~{rate} tries/sec)")
+            print(f"\n\nAll lines in {dictionary} tried and exhausted, password not found. You may try another dictionary file.")
             print("Press any key to exit.")
             input()
-
-        except ZeroDivisionError:
-            print("\n\nThe task completed successfully in zero seconds.")
-            print("Press any key to exit.")
-            input()
-    else:
-        print(f"\n\nAll lines in {dictionary} tried and exhausted, password not found. You may try another dictionary file.")
-        print("Press any key to exit.")
+    except KeyboardInterrupt:
+        clrscr()
+        print("\nCTRL ^C\n\nThrew a wrench in the works.")
+        print("Press Enter to exit.")
         input()
